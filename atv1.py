ea642fb0cc93ab418f601cbb8caa3e5126d1e5f0
Layssa <layssamoura19@gmail.com>
2023-06-15 20:40:29 -0300

    aula dia 15 de junho

--- atv1.py
+++ atv1.py
@@ -1,2 +1,23 @@
-print("Olá")
-print("Oi")
+# CLASSE
+
+class Pessoa:
+    
+    def __init__(self, nome, idade, cpf):
+        self.nome = nome                            #atributos
+        self.idade = idade
+        self.cpf = cpf
+    
+    def apresentar(self):                          #metodos
+        print("Olá, meu nome é: ",self.nome)
+    
+    def envelhecer(self, anos):
+        self.idade += anos
+        print("Minha idade é: ",self.idade)
+        
+    def mostrar_cpf(self):
+        print("Este é meu cpf: ",self.cpf)
+        
+pessoa1 = Pessoa("Layssa", 23, "12345678900")      #objeto
+pessoa1.apresentar()
+pessoa1.envelhecer(2)
+pessoa1.mostrar_cpf()
